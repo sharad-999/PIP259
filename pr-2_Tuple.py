b006f69922d93476f068f283698d9278908bc23f
Sharad kevadiya <sharadkevadiya777@gmail.com>
2022-01-08 22:00:04 +0530

    A completed

---
+++ pr-2_Tuple.py
@@ -0,0 +1,9 @@
+# --------------------  20CE042 - SHARAD KEVADIYA --------------------------
+# LINK : https://github.com/sharad-999/PIP259
+# ------------------------------   A   -----------------------------------------------
+# A. Write a Python program to create a tuple with different data types.
+# ------------------------------------------------------------------------------------
+
+# Tuples are used to store multiple items in a single variable.
+tuple = (1, "sharad", "20CE42", 4.2,True)
+print(tuple)
