805fac7fafacf29997dc6b119f63aae3ee319d0e
Sharad kevadiya <sharadkevadiya777@gmail.com>
2022-01-08 22:27:47 +0530

    B completed

--- pr-2_Tuple.py
+++ pr-2_Tuple.py
@@ -7,3 +7,14 @@
 # Tuples are used to store multiple items in a single variable.
 tuple = (1, "sharad", "20CE42", 4.2,True)
 print(tuple)
+
+
+# -----------------------------------   B   -----------------------------------------
+# b. Write a Python program to create a tuple with numbers and print one item.
+# -----------------------------------------------------------------------------------
+tup=(0,1,2,3,4)
+# specify index to print particular element of tuple
+print(tup[1])
+# tuple of one element
+tup1=80,
+print(tup1)
