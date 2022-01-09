01cca8afb0b9af777204be9a225bace24e1aeb19
Sharad kevadiya <sharadkevadiya777@gmail.com>
2022-01-09 19:32:00 +0530

    A completed

---
+++ pr-2_Set.py
@@ -0,0 +1,15 @@
+# --------------------  20CE042 - SHARAD KEVADIYA --------------------------
+# LINK : https://github.com/sharad-999/PIP259
+
+# ------------------------------   A   -----------------------------------------------
+# a. Write a Python program to add member(s) in a set and clear a set
+# ------------------------------------------------------------------------------------
+
+# set cotains only distinct value
+set= {2,3,4}
+# before adding element
+print(set)
+# add 5 in set
+set.add(5)
+# after adding element
+print(set)
