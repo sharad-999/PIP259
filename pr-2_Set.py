b1a33bceeed9c2dff1c8889c5628af82cbd9c5c3
Sharad kevadiya <sharadkevadiya777@gmail.com>
2022-01-09 19:46:55 +0530

    B completed

--- pr-2_Set.py
+++ pr-2_Set.py
@@ -13,3 +13,27 @@ print(set)
 set.add(5)
 # after adding element
 print(set)
+
+
+# -----------------------------------   B   -----------------------------------------
+# b. Write a Python program to remove an item from a set if it is present in the set.
+# -----------------------------------------------------------------------------------
+set={1,2,3,5,7}
+x=int(input("Enter Number to remove from Set :"))
+for i in set :
+    if(i==x):
+        set.remove(x)
+        print(x," removed from set")
+        print(set)
+        break
+    print(i)
+else:
+    print(x," Not found")
+
+#  --------  2nd Method :--------
+# if x in set :
+#     set.remove(x)
+#     print("Sucessfully removed")
+#     print(set)
+# else :
+#     print(x," Not Found in set")
