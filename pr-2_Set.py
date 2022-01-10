4d5b8b100fb6a7e68159d4037317005aa888f8ca
Sharad kevadiya <sharadkevadiya777@gmail.com>
2022-01-10 13:51:50 +0530

    E completed

--- pr-2_Set.py
+++ pr-2_Set.py
@@ -6,6 +6,9 @@
 # ------------------------------------------------------------------------------------
 
 # set cotains only distinct value
+from typing import List
+
+
 set= {2,3,4}
 # before adding element
 print(set)
@@ -60,3 +63,62 @@ print("difference of A and B :",A.difference(B))
 A={2,26,56,4,3,-2,55,0}
 print("Max in set :",max(A))
 print("Min in Set :",min(A))
+
+
+# -----------------------------------   E   -------------------------------------------------------------------
+# e. Write a Python program to find the most common elements and their counts from list, tuple, dictionary
+# -------------------------------------------------------------------------------------------------------------
+
+List = [2, 1, 2, 2, 1, 3]
+counter = 0
+num = List[0]
+for i in List:
+# The count() method returns the number of occurrences of a number in the given List.
+    curr_frequency = List.count(i)
+    if(curr_frequency> counter):
+        counter = curr_frequency
+        num = i
+print("Number which is repeted most: ")
+print(num)
+print("Frequency of most repeted number is: ")
+print(counter)
+
+# method 2
+# print(max(List,key=List.count))
+
+
+# count frequency in tuple
+tuple = (1, 2, 4, 5, 6, 6, 3, 6, 8, 3, 6)
+L=list(tuple)
+counter1 = 0
+num1 = L[0]
+for i in L:
+# The count() method returns the number of occurrences of a number in the given Tuple.
+    curr_frequency = L.count(i)
+    if(curr_frequency> counter1):
+        counter1 = curr_frequency
+        num1 = i
+print("Number which is repeted most: ")
+print(num1)
+print("Frequency of most repeted number is: ")
+print(counter1)
+
+
+# count frequency in Dictionary
+dic = {1: 'meet', 2: 'harmi', 3: 'sharad', 4: 'aniket', 5: 'jay',
+       6: 'sharad', 7: 'harmi', 8: 'sharad', 9: 'bhavdeep'}
+# values() method returns list of all values which are in dictionary
+value=dic.values()
+# convert values in list to count frequncy
+l1=list(value)
+counter1 = 0
+name= list(l1[0])
+for i in l1:
+    curr_frequency = l1.count(i)
+    if(curr_frequency> counter1):
+        counter1 = curr_frequency
+        name = i
+print("Name which is repeted most: ")
+print(name)
+print("Frequency of most repeted name is: ")
+print(counter1)
